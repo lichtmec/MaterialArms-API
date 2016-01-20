0439025f98f1e38760907b7d6e89d06a5d30ce29
licht <ulichit@gmail.com>
2016-01-21 00:24:46 +0900

    Set @Deprecated annotation
    EntityEffect system is still the prototype stage, so it is not recommended to implement this interface.

--- src/main/java/ma/api/eeffect/IEntityEffect.java
+++ src/main/java/ma/api/eeffect/IEntityEffect.java
@@ -1,10 +1,14 @@
 package ma.api.eeffect;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
 
+@Deprecated
 public interface IEntityEffect
 {
 	public boolean canApply (Entity target);
 
-	public void updateEffect (Entity entity);
+	public void updateEffect (Entity entity, NBTTagCompound data);
+
+	public void postUpdate (Entity entity, NBTTagCompound data);
 }
