7a720bb0e3c78b7a7b280e0db39fd94211116c80
licht <ulichit@gmail.com>
2016-02-05 01:33:13 +0900

    Renovation of Recipe-System. Interface for refer the recipe from the Underlying-Technology.

---
+++ src/main/java/ma/api/recipe/ITechUnlockable.java
@@ -0,0 +1,27 @@
+package ma.api.recipe;
+
+/**
+ * Information about U.Technology unlock.
+ * It is assumed to be implemented with IRecipeMA.
+ *
+ * @author	licht
+ */
+public interface ITechUnlockable
+{
+	/**
+	 * Check whether it is possible to unlock this U.Technology.
+	 *
+	 * @param tech	Check target
+	 *
+	 * @return	true(unlockable) or false(not unlockable)
+	 */
+	boolean isUnlockable (Technology.UnderlyingTechnology tech);
+
+	/**
+	 * Is secret info.
+	 * If the secret , it does not appear in the Research-Note.
+	 *
+	 * @return	true(secret) or false(not secret)
+	 */
+	boolean isSecret ();
+}
