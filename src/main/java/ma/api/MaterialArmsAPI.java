aab35b4d769f8148b6e454f02ce861036467691b
licht <ulichit@gmail.com>
2016-05-16 01:11:12 +0900

    Corresponding to MaterialArms - 0.9.5rc.

--- src/main/java/ma/api/MaterialArmsAPI.java
+++ src/main/java/ma/api/MaterialArmsAPI.java
@@ -1,5 +1,8 @@
 package ma.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Mod:MaterialArms API core
  * 
@@ -7,12 +10,33 @@ package ma.api;
  */
 public final class MaterialArmsAPI
 {
-	public static final String API_VER = "0.9.4";
+	public static final String API_VER = "0.9.5";
 
 	private static Boolean initialized = Boolean.FALSE;
+	private static Logger logger = LogManager.getLogger("MaterialArmsAPI");
 
 	public static boolean isInitialized ()
 	{
 		return initialized;
+	}
+
+	public static void sendLogInfo (String message)
+	{
+		logger.info(message);
+	}
+
+	public static void sendLogWarning (String message)
+	{
+		logger.warn(message);
+	}
+
+	public static void sendLogError (String message)
+	{
+		logger.error(message);
+	}
+
+	public static void sendLogFatal (String message)
+	{
+		logger.fatal(message);
 	}
 }
