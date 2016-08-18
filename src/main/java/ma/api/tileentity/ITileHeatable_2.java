37864a508c98cf54d938234c9730fd1cb910b373
licht <ulichit@gmail.com>
2016-08-19 02:08:50 +0900

    Add document to Heat-system api.

--- src/main/java/ma/api/tileentity/ITileHeatable_2.java
+++ src/main/java/ma/api/tileentity/ITileHeatable_2.java
@@ -1,11 +1,20 @@
 package ma.api.tileentity;
 
 /**
+ * Implement the concept of heat to TileEntity.<br>
+ * Machines of MaterialArms release heat to the surrounding ITileHeatable.
+ *
  * @author	licht
  */
 public interface ITileHeatable_2 extends ITileHeatable
 {
+	/**
+	 * @return	int value(set by ITileHeatable_2#setAcceptedHeatBuffer)
+	 */
 	int getAcceptedHeatBuffer ();
 
+	/**
+	 * @param amount	new buffer-param
+	 */
 	void setAcceptedHeatBuffer (int amount);
 }
