37f34b29bf9c97522f1273e08f03b62672ecf822
lichtmec <ulichit@gmail.com>
2020-06-08 12:22:19 +0900

    Add missing package-info.

---
+++ src/main/java/ma/api/item/enchant/package-info.java
@@ -0,0 +1,5 @@
+@API(apiVersion = MaterialArmsAPI.API_VER, owner = "MaterialArms", provides = "MaterialArmsAPI|itemenchant")
+package ma.api.item.enchant;
+
+import cpw.mods.fml.common.API;
+import ma.api.MaterialArmsAPI;
